64fdd78b3b33c6e36e63625093f3a23646c21301
kingofday <kingofday.b@gmail.com>
2022-06-10 18:25:20 +0430

    refactor buy-token

--- Source/src/components/index.jsx
+++ Source/src/components/index.jsx
@@ -7,7 +7,7 @@ import Login from "components/Auth/comps/Login";
 import RecoverPasword from "components/Auth/comps/RecoverPasword";
 import Signup from "components/Auth/comps/Signup";
 import Transaction from "./Panel/Transaction/transaction";
-import BuyToken from "./Panel/BuyToken/buyToken";
+import BuyToken from "./Panel/BuyToken";
 
 function App() {
   return (
