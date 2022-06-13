e3f71833368807135f51c2448d3fde1bb0c50915
ferizoozoo <farhadzohoorz@gmail.com>
2022-06-13 16:54:00 +0430

    Add ChangePassword form

--- Source/src/components/Panel/Profile/comps/ChangePassword.jsx
+++ Source/src/components/Panel/Profile/comps/ChangePassword.jsx
@@ -1,20 +1,67 @@
+import { Col, Form, Input, Row } from "antd";
+import { useChangePasswordMutation } from "api/account";
 import Button from "components/comps/Button";
-//import Input from "components/comps/Input";
+import { useState } from "react";
 import { useTranslation } from "react-i18next";
+import { useDispatch } from "react-redux";
 
 const ChangePassword = () => {
+  const [form] = Form.useForm();
   const { t } = useTranslation();
+  const [errorMsg, setErrorMsg] = useState("");
+
+  const dispatch = useDispatch();
+  const [changePassword, { isLoading, error, isError }] = useChangePasswordMutation();
+
+
+  const handleSubmit = async (values) => {
+    setErrorMsg("");
+    const call = await changePassword(values).unwrap();
+    if (!call.isSuccess) {
+      setErrorMsg(call.message);
+      return;
+    }
+  };
+
   return <div id="change-password">
-    <div id="input-fields">
-            {/* <div><Input /></div>
-            <div><Input /></div>
-            <div><Input /></div>
-            <div><Input /></div>
-            <div><Input /></div> */}
-        </div>
-        <div id="update-button">
-            <Button>{t("changePassword")}</Button>
-        </div>
+    <Form
+      form={form}
+      layout="vertical"
+      onFinish={handleSubmit}
+    >
+      <Row>
+        <Col xs={24}>
+          <Form.Item label={<span className="input-label">{t("oldPassword")}</span>}>
+            <Input.Password className="custom-input" />
+          </Form.Item>
+        </Col>
+      </Row>
+      <Row>
+        <Col xs={24}>
+          <Form.Item label={<span className="input-label">{t("newPassword")}</span>}>
+            <Input.Password className="custom-input" />
+          </Form.Item>
+        </Col>
+      </Row>
+      <Row>
+        <Col xs={24}>
+          <Form.Item label={<span className="input-label">{t("confirmNewPassword")}</span>}>
+            <Input.Password className="custom-input" />
+          </Form.Item>
+        </Col>
+      </Row>
+      <Row>
+        <Form.Item>
+          <Button
+            className="btn-primary w-100"
+            type="submit"
+            loading={isLoading}
+          >
+            {t("changePassword")}
+          </Button>
+        </Form.Item>
+      </Row>
+    </Form>
   </div>;
 };
 export default ChangePassword;
