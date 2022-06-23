fefd1ffb17c9e453e893f4c123459baedb628ac1
Mohammad Mokhtari <Mohammad.Mokhtari86@gmail.com>
2022-06-23 22:13:02 +0430

    Transaction Completed

--- Source/src/config/enums.js
+++ Source/src/config/enums.js
@@ -1,7 +1,20 @@
 export const networkTypes = {
-    Bitcoin: 1,
-    Ethereum: 2,
-    Solana: 3,
-    Tether: 4,
-    Binance: 5
-}
+  Bitcoin: 1,
+  Ethereum: 2,
+  Solana: 3,
+  Tether: 4,
+  Binance: 5,
+};
+
+export const transactionTypes = {
+  Deposit: 0,
+  Withdraw: 1,
+};
+
+export const transactionStatusTypes = {
+  Pending: 1,
+  TimedOut: 2,
+  RejectByUser: 3,
+  RejectByNetwork: 4,
+  ConfirmedByNetwork: 5,
+};
